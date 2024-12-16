159b4bddee1dc12ebf6792bc6c64af4610032bea
Melissa Jessen <s194322@student.dtu.dk>
2024-12-16 10:39:29 +0100

    Update

--- utils.py
+++ utils.py
@@ -16,30 +16,15 @@ from neuralop.datasets import load_darcy_flow_small
 from neuralop.utils import count_model_params
 
 
-def prepare_data(h_all, x, p_train, p_val):
-    x = x.squeeze()
+def prepare_data_2D(h_all, p_train, p_val):
     h_all = np.real(h_all)
 
     # Input a
     a = h_all[:-1]
-    a = a[:, np.newaxis, :]
+
     # Target u (one time step after a)
     u = h_all[1:]
-    u = u[:, np.newaxis, :]
-
-    mesh = x
-    mesh_shape_corrected = np.repeat(mesh[np.newaxis, np.newaxis, :], u.shape[0], axis=0)
-
-    # Concatenate a with mesh_shape_corrected
-    a_with_mesh = np.concatenate([a, mesh_shape_corrected], axis=1)
-    print(a_with_mesh.shape)
-
-    # Concatenate u with mesh_shape_corrected
-    u_with_mesh = np.concatenate([u, mesh_shape_corrected], axis=1)
-    print(u_with_mesh.shape)
-
-    n = len(a_with_mesh)
-    print(n)
+    n = len(h_all)
 
     # Split data into training, validation and test sets
     n_train = int(p_train * n)
@@ -58,7 +43,68 @@ def prepare_data(h_all, x, p_train, p_val):
 
     print(train_x.shape, train_y.shape, val_x.shape, val_y.shape, test_x.shape, test_y.shape)
 
-    return n_train, n_val, n_test, mesh, a_with_mesh, u_with_mesh, a, u, n, train_x, train_y, val_x, val_y, test_x, test_y
+    return n_train, n_val, n_test, a, u, n, train_x, train_y, val_x, val_y, test_x, test_y
 
+# Prepare data in sequences
+def prepare_data_sequences_2D(train_x, train_y, seq_length):
+    # Fetch the dimensions of the input data
+    timesteps, spatial_steps_x, spatial_steps_y = train_x.shape[0], train_x.shape[1], train_x.shape[2]
+
+    # Number of sequences
+    n_sequences = timesteps - seq_length
+
+    # Prepare input and target sequences
+    x_seq = torch.zeros((n_sequences, seq_length, spatial_steps_x, spatial_steps_y))
+    y_seq = torch.zeros((n_sequences, spatial_steps_x, spatial_steps_y))
+
+    for i in range(n_sequences):
+        x_seq[i] = train_x[i:i + seq_length]
+        y_seq[i] = train_y[i + seq_length]
+    return x_seq, y_seq
+
+
+def plot_losses(loss_train, loss_val, save, filename):
+    plt.figure(figsize=(7, 4))
+    plt.plot(loss_train, '.-', label='Training loss', linewidth=0.8, markersize=2)
+    plt.plot(loss_val, '.-', label='Validation loss', linewidth=0.8, markersize=2)
+    plt.yscale('log')
+    plt.legend()
+    plt.grid()
+    plt.title('Training and validation loss for each epoch')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss (MSE)')
+
+    if save:
+        plt.savefig(filename, format='pdf')
+
+    plt.show()
+
+def plot_error_2D(x, y, idx, t_all, pred_all, u, save, filename):
+    # Prepare grid
+    X, Y = np.meshgrid(x, y)
+    cmap = 'Blues_r'
+
+    fig = plt.figure(figsize=(12, 5))
+
+    ax = fig.add_subplot(121, projection='3d')
+    surf = ax.plot_surface(X, Y, np.abs(pred_all[idx]-u[idx]), cmap=cmap)
+    ax.set_title(f'Absolute error (m) at t = {t_all[idx]:.2f}')
+    ax.set_xlabel('Disatnce x (m)')
+    ax.set_ylabel('Distance y (m)')
+    ax.set_zlabel('Absolute error (m)')
+    #fig.colorbar(surf, ax=ax, shrink=0.8, aspect=10, pad=0.15)  # Colorbar for 3D plot
+
+    ax1 = fig.add_subplot(122)
+    heatmap = ax1.imshow(np.abs(pred_all[idx]-u[idx]), cmap=cmap)
+    ax1.set_title(f'Absolute error (m) at t = {t_all[idx]:.2f}')
+    ax1.set_xlabel('Distance x (m)')
+    ax1.set_ylabel('Distance y (m)')
+    fig.colorbar(heatmap, ax=ax1, shrink=0.8, aspect=10, pad = 0.15) # Add colorbar
+
+    plt.tight_layout()
+    if save:
+        plt.savefig(filename)
+
+    plt.show()
 
 
