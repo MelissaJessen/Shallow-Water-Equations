04edb0e810a017873acb6f7a7eaf9c11428e7c84
Melissa Jessen <s194322@student.dtu.dk>
2024-12-22 16:10:05 +0100

    Update files

--- functions_2D_FVM_SWE.py
+++ functions_2D_FVM_SWE.py
@@ -199,3 +199,105 @@ def solve_2D_SWE_FVM(Q, Qnew, t, tend, CFL, dx, dy, Nx, Ny):
 
 
 
+
+def solve_2D_SWE_FVM_constant_dt(Q, Qnew, t, tend, CFL, dx, dy, Nx, Ny):
+    Q_all = []
+    Q_all.append(Q)
+    t_all = []
+    t_all.append(t)
+
+     # Time loop
+    for n in range(100000):
+        sx = Lambdax(Q)  # characteristic velocities in x-direction
+        sy = Lambday(Q)  # characteristic velocities in y-direction
+        ax = np.max(np.abs(sx))
+        ay = np.max(np.abs(sy))
+        
+        #dt = CFL / (ax / dx + ay / dy)
+        dt = 0.025
+        if t + dt > tend:
+            dt = tend - t
+        if t >= tend:
+            break
+
+        # MUSCL part
+        slopeX = np.zeros_like(Q)
+        slopeY = np.zeros_like(Q)
+        
+        slopeX[:, 1:Nx-1, :] = minmodarray(Q[:, 1:Nx-1, :] - Q[:, 0:Nx-2, :], Q[:, 2:Nx, :] - Q[:, 1:Nx-1, :])
+        slopeY[:, :, 1:Ny-1] = minmodarray(Q[:, :, 1:Ny-1] - Q[:, :, 0:Ny-2], Q[:, :, 2:Ny] - Q[:, :, 1:Ny-1])
+
+        Qxm = Q - 0.5 * slopeX
+        Qxp = Q + 0.5 * slopeX
+        Qym = Q - 0.5 * slopeY
+        Qyp = Q + 0.5 * slopeY
+
+        Q_t = -(FluxX(Qxp) - FluxX(Qxm)) / dx - (FluxY(Qyp) - FluxY(Qym)) / dy
+        Qxm += 0.5 * dt * Q_t
+        Qxp += 0.5 * dt * Q_t
+        Qym += 0.5 * dt * Q_t
+        Qyp += 0.5 * dt * Q_t
+
+        # Recompute physical fluxes and characteristic velocities for Rusanov flux
+        fxm, fxp = FluxX(Qxm), FluxX(Qxp)
+        gym, gyp = FluxY(Qym), FluxY(Qyp)
+        
+        sxm, sxp = Lambdax(Qxm), Lambdax(Qxp)
+        sym, syp = Lambday(Qym), Lambday(Qyp)
+
+        dtdx = dt / dx
+        dtdy = dt / dy
+
+        # Space loop
+        for i in range(Nx):
+            for j in range(Ny):
+                # Numerical fluxes in X
+                if i == 0:
+                    Qghost = Q[:, i, j].reshape(3, 1, 1).copy()  # Reshape to (3, 1, 1)
+                    Qghost[1] = -Qghost[1]
+                    Fm = Rusanov(Qghost, Qxm[:, i, j], FluxX(Qghost), fxm[:, i, j], Lambdax(Qghost), sxm[:, i, j])
+                    Fp = Rusanov(Qxp[:, i, j], Qxm[:, i+1, j], fxp[:, i, j], fxm[:, i+1, j], sxp[:, i, j], sxm[:, i+1, j])
+                elif i == Nx - 1:
+                    Qghost = Q[:, i, j].reshape(3, 1, 1).copy()  # Reshape to (3, 1, 1)
+                    Qghost[1] = -Qghost[1]
+                    Fm = Rusanov(Qxp[:, i-1, j], Qxm[:, i, j], fxp[:, i-1, j], fxm[:, i, j], sxp[:, i-1, j], sxm[:, i, j])
+                    Fp = Rusanov(Qxp[:, i, j], Qghost, fxp[:, i, j], FluxX(Qghost), sxp[:, i, j], Lambdax(Qghost))
+                else:
+                    Fm = Rusanov(Qxp[:, i-1, j], Qxm[:, i, j], fxp[:, i-1, j], fxm[:, i, j], sxp[:, i-1, j], sxm[:, i, j])
+                    Fp = Rusanov(Qxp[:, i, j], Qxm[:, i+1, j], fxp[:, i, j], fxm[:, i+1, j], sxp[:, i, j], sxm[:, i+1, j])
+
+                # Numerical fluxes in Y
+                if j == 0:
+                    Qghost = Q[:, i, j].reshape(3, 1, 1).copy()  # Reshape to (3, 1, 1)
+                    Qghost[2] = -Qghost[2]
+                    Gm = Rusanov(Qghost, Qym[:, i, j], FluxY(Qghost), gym[:, i, j], Lambday(Qghost), sym[:, i, j])
+                    Gp = Rusanov(Qyp[:, i, j], Qym[:, i, j+1], gyp[:, i, j], gym[:, i, j+1], syp[:, i, j], sym[:, i, j+1])
+                elif j == Ny - 1:
+                    Qghost = Q[:, i, j].reshape(3, 1, 1).copy()  # Reshape to (3, 1, 1)
+                    Qghost[2] = -Qghost[2]
+                    Gm = Rusanov(Qyp[:, i, j-1], Qym[:, i, j], gyp[:, i, j-1], gym[:, i, j], syp[:, i, j-1], sym[:, i, j])
+                    Gp = Rusanov(Qyp[:, i, j], Qghost, gyp[:, i, j], FluxY(Qghost), syp[:, i, j], Lambday(Qghost))
+                else:
+                    Gm = Rusanov(Qyp[:, i, j-1], Qym[:, i, j], gyp[:, i, j-1], gym[:, i, j], syp[:, i, j-1], sym[:, i, j])
+                    Gp = Rusanov(Qyp[:, i, j], Qym[:, i, j+1], gyp[:, i, j], gym[:, i, j+1], syp[:, i, j], sym[:, i, j+1])
+
+                # Finite volume update
+                Qnew[:, i, j] = Q[:, i, j] - dtdx * (Fp - Fm) - dtdy * (Gp - Gm)
+                
+
+        # Update time and solution
+        t += dt
+        # Print t with 3 decimals
+        print(f't = {t:.3f}')
+        t_all.append(t)
+        Q = np.copy(Qnew)
+        Q_all.append(Q)
+
+    n = len(Q_all)
+    print(f'There are time steps: {n}')
+
+    return Q_all, t_all
+
+
+
+
